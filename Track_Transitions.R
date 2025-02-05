0b134a4634a6d05e1b13dc3b140143858bf33c76
chbianco <christopherhbianco@gmail.com>
2025-02-05 15:16:38 -0500

    Update Track_Transitions.R

--- Track_Transitions.R
+++ Track_Transitions.R
@@ -40,7 +40,7 @@ transitions %>%
   summarize(sum(sqkm_change)) %>%
   rename(total_skqm_change = `sum(sqkm_change)`) %>%
   arrange(desc(total_skqm_change)) %>%
-  filter(total_skqm_change > 500000)-> total_transitions
+  filter(total_skqm_change > 1020000)-> total_transitions
 
 #Sort and sum land use transition by GCAM32 region
 transitions %>%
